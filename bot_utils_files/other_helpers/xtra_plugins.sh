0188153724702f729919ff992f6cd098d7eb2efb
mohsinhsn <84607974+mohsinhsn@users.noreply.github.com>
2021-07-07 23:13:04 +0600

    Update xtra_plugins.sh

--- bot_utils_files/other_helpers/xtra_plugins.sh
+++ bot_utils_files/other_helpers/xtra_plugins.sh
@@ -1,12 +1,12 @@
-# Copyright (C) 2020-2021 by DevsExpo@Github, < https://github.com/DevsExpo >.
+# Copyright (C) 2020-2021 by Mohsinhsn@Github, < https://github.com/Mohsinhsn >.
 #
-# This file is part of < https://github.com/DevsExpo/FridayUserBot > project,
+# This file is part of < https://github.com/mohsinhsn/StylishUserBot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/DevsExpo/blob/master/LICENSE >
+# Please see < https://github.com/MohsinHsn/blob/master/LICENSE >
 #
 # All rights reserved.
 
-PLUGIN_REPO="https://github.com/DevsExpo/Xtra-Plugins.git"
+PLUGIN_REPO="https://github.com/mohsinhsn/Xtra-Plugins.git"
 xtra_fold="./xtraplugins"
 req_file="./xtraplugins/req.txt"
 

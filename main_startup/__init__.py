46540c9ff214d8335ebe9a77024cf8b3f3d88088
mohsinhsn <84607974+mohsinhsn@users.noreply.github.com>
2021-07-08 01:17:45 +0600

    Update __init__.py

--- main_startup/__init__.py
+++ main_startup/__init__.py
@@ -1,8 +1,8 @@
-# Copyright (C) 2020-2021 by DevsExpo@Github, < https://github.com/DevsExpo >.
+# Copyright (C) 2020-2021 by MohsinHsn@Github, < https://github.com/MohsinHsn >.
 #
-# This file is part of < https://github.com/DevsExpo/FridayUserBot > project,
+# This file is part of < https://github.com/MohsinHsn/StylishUserBot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/DevsExpo/blob/master/LICENSE >
+# Please see < https://github.com/MohsinHsn/blob/master/LICENSE >
 #
 # All rights reserved.
 
@@ -35,57 +35,57 @@ XTRA_CMD_LIST = {}
 sudo_id = Config.AFS
 
 if not Config.STRINGSESSION:
-    logging.error("No String Session Found! Friday is Exiting!")
+    logging.error("No String Session Found! Stylish is Exiting!")
     quit(1)
 
 if not Config.API_ID:
-    logging.error("No Api-ID Found! Friday is Exiting!")
+    logging.error("No Api-ID Found! Stylish is Exiting!")
     quit(1)
 
 if not Config.API_HASH:
-    logging.error("No ApiHash Found! Friday is Exiting!")
+    logging.error("No Api-Hash Found! Stylish is Exiting!")
     quit(1)
 
 if not Config.LOG_GRP:
-    logging.error("No Log Group ID Found! Friday is Exiting!")
+    logging.error("No Log Group ID Found! Stylish is Exiting!")
     quit(1)
 
 
 # Clients - Upto 4 Clients is Supported!
 if Config.STRINGSESSION:
-    Friday = Client(
+    Stylish = Client(
         Config.STRINGSESSION,
         api_id=Config.API_ID,
         api_hash=Config.API_HASH,
         sleep_threshold=180,
     )
 if Config.STRINGSESSION_2:
-    Friday2 = Client(
+    Stylish2 = Client(
         Config.STRINGSESSION_2,
         api_id=Config.API_ID,
         api_hash=Config.API_HASH,
         sleep_threshold=180,
     )
 else:
-    Friday2 = None
+    Stylish2 = None
 if Config.STRINGSESSION_3:
-    Friday3 = Client(
+    Stylish3 = Client(
         Config.STRINGSESSION_3,
         api_id=Config.API_ID,
         api_hash=Config.API_HASH,
         sleep_threshold=180,
     )
 else:
-    Friday3 = None
+    Stylish3 = None
 if Config.STRINGSESSION_4:
-    Friday4 = Client(
+    Stylish4 = Client(
         Config.STRINGSESSION_4,
         api_id=Config.API_ID,
         api_hash=Config.API_HASH,
         sleep_threshold=180,
     )
 else:
-    Friday4 = None
+    Stylish4 = None
 
 if Config.BOT_TOKEN:
     bot = Client(

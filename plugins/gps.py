a49fdf0485e29302b013b6f811f5d2350736e005
mohsinhsn <84607974+mohsinhsn@users.noreply.github.com>
2021-07-08 23:07:56 +0600

    Update gps.py

--- plugins/gps.py
+++ plugins/gps.py
@@ -1,8 +1,8 @@
-# Copyright (C) 2020-2021 by DevsExpo@Github, < https://github.com/DevsExpo >.
+# Copyright (C) 2020-2021 by mohsinhsn@Github, < https://github.com/mohsinhsn >.
 #
-# This file is part of < https://github.com/DevsExpo/FridayUserBot > project,
+# This file is part of < https://github.com/mohsinhsn/StylishUserBot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/DevsExpo/blob/master/LICENSE >
+# Please see < https://github.com/mohsinhsn/blob/master/LICENSE >
 #
 # All rights reserved.
 
@@ -11,13 +11,13 @@ import os
 
 from geopy.geocoders import Nominatim
 
-from main_startup.core.decorators import friday_on_cmd
+from main_startup.core.decorators import stylish_on_cmd
 from main_startup.helper_func.basic_helpers import edit_or_reply, get_text
 
 GMAPS_LOC = "https://maps.googleapis.com/maps/api/geocode/json"
 
 
-@friday_on_cmd(
+@stylish_on_cmd(
     ["gps"],
     cmd_help={
         "help": "Find and send the given location",
@@ -31,7 +31,7 @@ async def gps(client, message):
         await pablo.edit("Please Provide location for sending GPS")
         return
     try:
-        geolocator = Nominatim(user_agent="FridayUB")
+        geolocator = Nominatim(user_agent="StylishUB")
         location = args
         geoloc = geolocator.geocode(location)
         longitude = geoloc.longitude

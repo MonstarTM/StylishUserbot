fe66548dfd3d7b57e0bb20654a72dcbb78da7fbe
mohsinhsn <84607974+mohsinhsn@users.noreply.github.com>
2021-07-08 21:09:16 +0600

    Update rename.py

--- plugins/rename.py
+++ plugins/rename.py
@@ -1,18 +1,18 @@
-# Copyright (C) 2020-2021 by DevsExpo@Github, < https://github.com/DevsExpo >.
+# Copyright (C) 2020-2021 by mohsinhsn@Github, < https://github.com/mohsinhsn >.
 #
-# This file is part of < https://github.com/DevsExpo/FridayUserBot > project,
+# This file is part of < https://github.com/mohsinhsn/stylishUserBot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/DevsExpo/blob/master/LICENSE >
+# Please see < https://github.com/mohsinhsn/blob/master/LICENSE >
 #
 # All rights reserved.
 
 import time
 
-from main_startup.core.decorators import friday_on_cmd
+from main_startup.core.decorators import stylish_on_cmd
 from main_startup.helper_func.basic_helpers import edit_or_reply, get_text, progress
 
 
-@friday_on_cmd(
+@stylish_on_cmd(
     ["rename", "rupload"],
     cmd_help={
         "help": "Rename File!",
@@ -54,5 +54,5 @@ async def rename(client, message):
         progress_args=(pablo, c_time, f"`Uploading {fname}`", EsCoBaR),
     )
     await pablo.edit(
-        "File Renamed and Uploaded. By FridayUserBot. Get your FridayUserBot from @fridaychat"
+        "File Renamed and Uploaded. By StylishUserBot. Get your StylishUserBot from @stylishUser"
     )

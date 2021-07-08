fad60dd26204d1ae50a304da8bff72cc4cbbf306
mohsinhsn <84607974+mohsinhsn@users.noreply.github.com>
2021-07-08 17:56:07 +0600

    Update string_gen.py

--- string_gen.py
+++ string_gen.py
@@ -15,7 +15,7 @@ print(stylish_)
 api_id = input("Enter Your API ID: \n")
 api_hash = input("Enter Your API HASH : \n")
 
-with Client("FridayUB", api_id=api_id, api_hash=api_hash) as bot_:
+with Client("StylishUB", api_id=api_id, api_hash=api_hash) as bot_:
     first_name = (bot_.get_me()).first_name
     string_session_ = f"<b><u>String Session For {first_name}</b></u> \n<code>{bot_.export_session_string()}</code>"
     bot_.send_message("me", string_session_, parse_mode="html")

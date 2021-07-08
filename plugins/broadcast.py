0e7cd48162cbeabb2364ae75da8a7ea264bec395
mohsinhsn <84607974+mohsinhsn@users.noreply.github.com>
2021-07-08 22:35:44 +0600

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -1,8 +1,8 @@
-# Copyright (C) 2020-2021 by DevsExpo@Github, < https://github.com/DevsExpo >.
+# Copyright (C) 2020-2021 by mohsinhsn@Github, < https://github.com/mohsinhsn >.
 #
-# This file is part of < https://github.com/DevsExpo/FridayUserBot > project,
+# This file is part of < https://github.com/mohsinhsn/stylishUserBot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/DevsExpo/blob/master/LICENSE >
+# Please see < https://github.com/mohsinhsn/blob/master/LICENSE >
 #
 # All rights reserved.
 
@@ -14,15 +14,15 @@ from database.broadcast_db import (
     is_broadcast_chat_in_db,
     rmbroadcast_chat,
 )
-from main_startup.core.decorators import friday_on_cmd
+from main_startup.core.decorators import stylish_on_cmd
 from main_startup.helper_func.basic_helpers import edit_or_reply, get_text
 
 
-@friday_on_cmd(
+@stylish_on_cmd(
     ["badd"],
     cmd_help={
         "help": "Add Group/Channel For Broadcast!. Give input as 'all' to add all.",
-        "example": "{ch}badd @fridaysupportofficial",
+        "example": "{ch}badd @stylishuser",
     },
 )
 async def badd(client, message):
@@ -61,11 +61,11 @@ async def badd(client, message):
         await pablo.edit(f"`Successfully Added {bd} in dB!`")
 
 
-@friday_on_cmd(
+@stylish_on_cmd(
     ["brm"],
     cmd_help={
         "help": "Remove Group/Channel From Broadcast dB!. Give input as 'all' to Remove all.",
-        "example": "{ch}brm @fridaysupportofficial",
+        "example": "{ch}brm @stylishUser",
     },
 )
 async def brm(client, message):
@@ -94,7 +94,7 @@ async def brm(client, message):
         await pablo.edit(f"`Successfully Added {bd} in dB!`")
 
 
-@friday_on_cmd(
+@stylish_on_cmd(
     ["broadcast"],
     cmd_help={
         "help": "Broadcast Message In All Groups/Channels which are added in dB.",

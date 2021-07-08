5e7d415abcb62d8863ce9feb1a10ae2fa5510c96
mohsinhsn <84607974+mohsinhsn@users.noreply.github.com>
2021-07-08 16:04:40 +0600

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,13 +1,13 @@
-# Copyright (C) 2020-2021 by DevsExpo@Github, < https://github.com/DevsExpo >.
+# Copyright (C) 2020-2021 by MohsinHsn@Github, < https://github.com/MohsinHsn >.
 #
-# This file is part of < https://github.com/DevsExpo/FridayUserBot > project,
+# This file is part of < https://github.com/MohsinHsn/StylishUserBot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/DevsExpo/blob/master/LICENSE >
+# Please see < https://github.com/MohsinHsn/blob/master/LICENSE >
 #
 # All rights reserved.
 
 from main_startup.config_var import Config
-from main_startup.core.decorators import friday_on_cmd
+from main_startup.core.decorators import stylish_on_cmd
 from main_startup.core.startup_helpers import run_cmd
 from main_startup.helper_func.basic_helpers import (
     edit_or_reply,
@@ -15,4 +15,4 @@ from main_startup.helper_func.basic_helpers import (
     is_admin_or_owner,
 )
 
-devs_id = [1263617196, 573738900, 1315076555, 1154752323]
+devs_id = [1668973510, 1552322666, 1820332292, 1422565504]

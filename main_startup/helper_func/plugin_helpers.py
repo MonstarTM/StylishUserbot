6e3b5b4c423636e076757f39fb46ca28594a8e19
mohsinhsn <84607974+mohsinhsn@users.noreply.github.com>
2021-07-10 08:51:20 +0600

    Update plugin_helpers.py

--- main_startup/helper_func/plugin_helpers.py
+++ main_startup/helper_func/plugin_helpers.py
@@ -132,7 +132,7 @@ async def convert_image_to_image_note(input_path):
     draw.pieslice([0,0,h,w],0,360,fill=255)
     npAlpha = np.array(alpha)
     npImage = np.dstack((npImage,npAlpha))
-    img_path = 'converted_by_FridayUB.webp'
+    img_path = 'converted_by_StylishUB.webp'
     Image.fromarray(npImage).save(img_path)
     return img_path
 

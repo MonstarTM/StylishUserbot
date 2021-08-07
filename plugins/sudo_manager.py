ee63e25007a7e2bf22b9d71cfb0a5d0a71167513
mohsinhsn <84607974+mohsinhsn@users.noreply.github.com>
2021-08-07 14:26:19 +0600

    Update sudo_manager.py

--- plugins/sudo_manager.py
+++ plugins/sudo_manager.py
@@ -1,5 +1,3 @@
-# All rights reserved.
-
 
 from pyrogram import filters
 
@@ -18,7 +16,7 @@ from plugins import devs_id
 
 
 @stylish_on_cmd(['addsudo'],
-              disable_sudo=False,
+              disable_sudo=true,
               cmd_help={
                 "help": "Add User To Sudo List.",
                 "example": "{ch}addsudo (reply_to_user)",
@@ -45,7 +43,7 @@ async def add_s_sudo(client, message):
     await msg_.edit(engine.get_string("ADDED_TO_SUDO").format(user.mention))
     
 @stylish_on_cmd(['rmsudo'],
-              disable_sudo=False,
+              disable_sudo=true,
               cmd_help={
                 "help": "Remove User From Sudo List.",
                 "example": "{ch}rmsudo (reply_to_user)",
